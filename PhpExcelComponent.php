d949366a240f9f661a38012332a2d2ab68bd7969
ksgaira <ksgaira@AG-DT-044.Avalon>
2016-02-01 12:59:11 +0530

    New functions

--- PhpExcelComponent.php
+++ PhpExcelComponent.php
@@ -5,10 +5,10 @@ class PhpExcelComponent extends Component {
     private $phpExcelName = 'PHPExcel';
     private $objPHPExcel = null, $objWorksheet = null;
     private $inputFileType = 'Excel2007';
-    private $defaults = array('extension' => '.xlsx', 'excelName' => 'ExcelSheet','sheet1Name'=>'Sheet1');
+    private $defaults = array('extension' => '.xlsx', 'excelName' => 'ExcelSheet', 'sheet1Name' => 'Sheet1');
+    private $alphabets = null;
 
-   
-     /*
+    /*
      * Create workbook,and return 
      */
 
@@ -38,7 +38,6 @@ class PhpExcelComponent extends Component {
                 $objPHPExcel->createSheet($i);
                 $objPHPExcel->setActiveSheetIndex($i);
                 $objPHPExcel->getActiveSheet()->setTitle('Sheet' . ($i + 1));
-                
             }
             $this->objWorksheet = $objPHPExcel->getActiveSheet(); // Make object accessable globally
         }
@@ -59,11 +58,55 @@ class PhpExcelComponent extends Component {
         }
         $fileName = $fileName . $this->defaults['extension'];  // Full name
 
-        header('Content-type: application/vnd.ms-excel');
+        header('Content-type: application/vnd.openxmlformats-officedocument.spreadsheetml.sheet');
         header('Content-Disposition: attachment; filename=' . $fileName);
         // Write file to the browser
         $objWriter->save('php://output');
         die;
+    }
+
+    /*
+     * ************** Style related functions*****************************
+     */
+    /*     * *****Fill colour of a particular cell.Colour code in hex value without "#" *********** */
+
+    function fillCellColour($cellDim, $color) {
+        $cellBckStyle = array(
+            'fill' => array(
+                'type' => PHPExcel_Style_Fill::FILL_SOLID,
+                'color' => array('rgb' => $color)
+        ));
+        $this->objWorksheet->getStyle($cellDim)->applyFromArray($cellBckStyle); // fill colour
+    }
+
+    /*     * *****Draw line.For example botton line for range "A3:A20" *********** */
+
+    // Allowed type: "bottom","right","left","top"
+    function drawLine($lineRange, $type, $colour = 'D9D9D9') {
+        $border_style = array('borders' => array($type =>
+                array('style' =>
+                    PHPExcel_Style_Border::BORDER_THIN,
+                    'color' => array('rgb' => $colour)
+        )));
+        $this->objWorksheet->getStyle($lineRange)->applyFromArray($border_style);
+    }
+
+    function addComment($cellDim, $comment) {
+        $this->objWorksheet->getComment($cellDim)->getText()->createTextRun($comment); // Add comment
+    }
+
+    function _writeCellValue($cell, $value) {
+        $this->objWorksheet->getCell($cell)->setValue($value);
+    }
+
+    public function _alphabets($level) {
+        //  Alphabets Array
+        $this->alphabets = $alphabets = range('A', 'Z'); // Array containing latters from A to Z
+        for ($i = 0; $i < $level; $i++) {
+            foreach ($alphabets as $alpha) {
+                array_push($this->alphabets, $alphabets[$i] . $alpha);
+            }
+        }
     }
 
     function getExcelObj() {
